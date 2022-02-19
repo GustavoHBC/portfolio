95badfd72ec48e74655ba170626afbf99523cf7c
GustavoHBC <gus14.dev@gmail.com>
2022-02-19 16:25:55 -0300

    feat: adding broxas subdomain
    Signed-off-by: GustavoHBC <gus14.dev@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,9 +1,34 @@
 /** @type {import('next').NextConfig} */
+
+const sites = {
+  portfolio: {
+    domain: 'gushbc.dev'
+  },
+  broxas: {
+    domain: 'broxas.gushbc.dev'
+  },
+};
+
+const siteKeys = Object.keys(sites);
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['github.com']
+  },
+  publicRuntimeConfig: {
+    sites
+  },
+  i18n: {
+    domains: siteKeys.map(siteKey => (
+      {
+        domain: sites[siteKey].domain,
+      }
+    ))
+  },
+  future: {
+    webpack5: true
   }
-}
+};
 
 module.exports = nextConfig
