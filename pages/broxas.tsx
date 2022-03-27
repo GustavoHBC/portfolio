38836c1ec854670754f9c23db0d28ee8f3613201
GustavoHBC <gus14.dev@gmail.com>
2022-03-27 00:40:59 -0300

    patch: optimizing data fetching and rendering
    Signed-off-by: GustavoHBC <gus14.dev@gmail.com>

--- pages/broxas.tsx
+++ pages/broxas.tsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react';
 import type { NextPage } from 'next';
 import { Heading, SimpleGrid } from '@chakra-ui/react';
 
@@ -6,57 +5,56 @@ import Layout from '../components/layout';
 import StatsCard from 'components/stats-card';
 import ScorePanel from 'components/score-panel';
 
-type TGames = {
-  name: string
-  seniority: string
-  description: string
-  image: string
-  game: {
-    id: number
+interface IMatches {
+  name?: string
+  seniority?: string
+  description?: string
+  image?: string
+  match?: {
+    id?: number
     team1: {
-      score: number
-      name: string
+      score?: number
+      name?: string
     }
     team2: {
-      score: number
-      name: string
+      score?: number
+      name?: string
     }
   }
 }
 
-const JsFight: NextPage = () => {
-  const [games, setGames] = useState<Array<TGames>>([]);
-  
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch('/api/broxas', {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json"
-        },
-      });
+export async function getServerSideProps() {
+  let matches: IMatches[] = [];
+  const fetchData = async () => {
+    const response = await fetch('/api/broxas', {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      },
+    });
 
-      if (!response.ok) {
-        throw new Error(`Error: ${response.status}`);
-      }
-
-      const gamesJson = await response.json();
-      setGames(gamesJson);
-      // setGames([]);
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
     }
 
-    fetchData();
-  }, []);
+    matches = await response.json();;
+  };
+  await fetchData();
+  return {
+    props: {matches}, // will be passed to the page component as props
+  }
+}
 
+const JsFight: NextPage<IMatches> = (props) => {
   return (
     <Layout>
       <Heading as="h2" variant="page-title" textAlign="center">
         Inimigos do Javascript
       </Heading>
       <SimpleGrid columns={2} minChildWidth='320px' spacingX="20px" spacingY="20px" py={4}>
-        {Array.isArray(games) && games.map(({game, ...data}, index) => (
+        {Array.isArray(props) && props.map(({match, ...data}, index) => (
           <StatsCard key={`jsfight-${index}`} {...data}>
-            <ScorePanel {...game} />
+            <ScorePanel {...match} />
           </StatsCard>
         ))}
       </SimpleGrid>
